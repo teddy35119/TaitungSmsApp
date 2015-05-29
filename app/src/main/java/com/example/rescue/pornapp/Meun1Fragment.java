62ed336afd2a23de049500f77bf2956ac4838dbb
teddy35119 <teddy35119@gmail.com>
2015-05-29 10:32:01 +0800

    1.add header 2.item_selec & color

--- app/src/main/java/com/example/rescue/pornapp/Meun1Fragment.java
+++ app/src/main/java/com/example/rescue/pornapp/Meun1Fragment.java
@@ -3,45 +3,41 @@ package com.example.rescue.pornapp;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 /**
  * Created by teddy on 2015/5/14.
  */
 public class Meun1Fragment extends Fragment {
-    View RootView;
-    private ListView PhoneNumberListView;
-    private TextView NameText,NumberText;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
-
+    View RootView;
+    private ListView PhoneNumberListView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         RootView = inflater.inflate(R.layout.meun1_fragment,container,false);
         //自己塞的資料
-        String [] Name = new String[]{"熊德麟","林宏逸","溫立言","施得群","胡維軒"};
-        String [] Number = new String[]{"0970562218","0937083654","0928504697","0987424260","000"};
+        String [] Name = new String[]{"陳oo","林oo","溫oo","施oo","胡oo"};
+        String [] Number = new String[]{"0970135531","0937232323","0928123456","0987987789","0988123423"};
         PhoneNumberListView = (ListView) RootView.findViewById(R.id.listView);
-        NameText = (TextView) RootView.findViewById(R.id.PhoneCallName);
-        NumberText = (TextView) RootView.findViewById(R.id.PhoneCallNumber);
+        //取得header
+        View header = inflater.inflate(R.layout.listviewsms_header, PhoneNumberListView, false);
+        PhoneNumberListView.addHeaderView(header, null, false);
+        //自訂layout頁面並利用Baseadater來轉接
+        PhoneNumberListView.setAdapter(new PhoneNumberAdapter(getActivity(),Name,Number));
+
         //用android本身設計得好的樣式
         //ArrayAdapter aa= new ArrayAdapter(getActivity(),android.R.layout.simple_expandable_list_item_1,value);
-        //自訂的layout利用Baseadater來轉接
-        PhoneNumberListView.setAdapter(new PhoneNumberAdapter(getActivity(),Name,Number));
-        PhoneNumberListView.addHeaderView(NameText);
-        PhoneNumberListView.addHeaderView(NumberText);
+
+
 
         return RootView;
     }
